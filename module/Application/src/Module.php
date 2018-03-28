7e8152ae20779cd09c8495a1838714e744b28865
cristian.incarnato@sondeos.com.ar <Neverwinter2.!>
2018-03-28 11:02:19 -0300

    Fix Application Layout

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -3,14 +3,15 @@
 namespace Application;
 
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\ModuleManager;
 
 
 class Module implements ConfigProviderInterface {
 
-    const VERSION = '3.0.2dev';
-
     public function getConfig() {
         return include __DIR__ . '/../config/module.config.php';
     }
+
+
 
 }
