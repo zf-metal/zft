8b5ffa9c053a9a49c282e0cc4f02b607f99cc9e0
crist <cristian.cdi@gmail.com>
2018-03-27 23:15:22 -0300

    guest role

--- config/autoload/zfc_rbac.global.php
+++ config/autoload/zfc_rbac.global.php
@@ -2,9 +2,10 @@
 
 return [
     'zfc_rbac' => [
+        'guest_role' => 'guest',
         'guards' => [
             'ZfcRbac\Guard\RouteGuard' => [
-                 'Application*' => ['user'],
+                'Application*' => ['user'],
             ]
         ],
     ]
