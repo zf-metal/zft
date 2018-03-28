f7c9e8f80438c2cb08c623a665d4d3687df0d850
cristian.incarnato@sondeos.com.ar <Neverwinter2.!>
2018-03-28 11:15:03 -0300

    Fix

--- config/autoload/security.global.php
+++ config/autoload/security.global.php
@@ -11,7 +11,7 @@ return [
         'user_state_default' => true,
         'password_recovery' => true,
         'bcrypt_cost' => 12,
-        'profile_picture_path' => __DIR__ . '/../public/img/profile/',
+        'profile_picture_path' => __DIR__ . '/../../public/img/profile/',
         'profile_picture_path_relative' => '/img/profile/',
         'saved_user_redirect_route' => 'zf-metal.admin/users/view',
         'redirect_strategy' => [
