5831aec5fbc12badfc110f148088ba1adbaff8ef
cristian.cdi@gmail.com <Neverwinter2>
2018-11-30 16:57:29 -0300

    Cascade remove

---
+++ config/autoload/zfm-email-campaigns.global.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+  'ZfMetal\EmailCampaigns.options' => [
+    'url_domain'            => 'http://zft.int/',
+    'search_campaign_limit' => 5,
+    'path_attached_files'   => './files'
+  ]
+];
